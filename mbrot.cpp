1c0c3a889260ffe04a02f65b9c40a3f1f2cedfb7
AIt-F4 <tierneyluca@gmail.com>
2023-10-09 14:04:02 +0100

    computeMandelbrot implemented

--- mbrot.cpp
+++ mbrot.cpp
@@ -5,20 +5,42 @@
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_real_distribution<double> real(-2,1); // real range
-std::uniform_real_distribution<double> imaginary(-1.5,1.5); // irrational range
+std::uniform_real_distribution<double> imaginary(-1.5,1.5); // imaginary range
 
-//displays the complex number supplied
-// void display(complex <double> c){
-//    std::cout << "The complex number is: ";
-//    std::cout << real(c) << '+' << imag(c) << 'i' << std::endl;
-// }
+MandelbrotPointInfo compute_mandelbrot(complex<double> c, int max_iters, bool collect_points) 
+{
+    MandelbrotPointInfo pointInfo = {};
+    pointInfo.initial_point = c;
+    pointInfo.points_in_path = {};
+    pointInfo.max_iters = max_iters;
 
+    std::complex<double> z = std::complex(0.0,0.0);
+    for (int i = 1 ;i <= max_iters; i++)
+    {
+        z = pow(z,2) + c;
+        pointInfo.points_in_path.push_back(z);
+        if ((pow(z.real(),2) +pow(z.imag(),2)) > 4.0)
+        {
+            pointInfo.num_iters = i;
+            pointInfo.escaped = true;
+            return pointInfo;
+        }
+    }
 
+    pointInfo.escaped = false;
+    pointInfo.num_iters = max_iters;
+    return pointInfo;
+}
 
 int main(){
     double c_real = real(gen);
     double c_imaginary = imaginary(gen); 
-    std::complex<double> c = c_real + c_imaginary;
-    std::cout << c;
-    // display(c);
+
+    std::complex c = std::complex(c_real ,c_imaginary);
+    // std::cout << c << std::endl << std::pow(c,2) << std::endl;
+    // std::cout << c.real();
+    MandelbrotPointInfo cPointInfo = compute_mandelbrot(c,10,true);
+    std::cout << cPointInfo.initial_point << std::endl << cPointInfo.escaped << std::endl << cPointInfo.num_iters << std::endl <<cPointInfo.max_iters << std::endl;
+
+
 }
