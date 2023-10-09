1d3adc1d9d090a4a68f61652acabbf14671ca4cd
AIt-F4 <tierneyluca@gmail.com>
2023-10-09 10:53:29 +0100

    random complex number successfully generated

--- mbrot.cpp
+++ mbrot.cpp
@@ -1,18 +1,24 @@
-//#include <mbrot.h>
+#include "mbrot.h"
 #include <iostream>
 #include <random>
-#include <vector>
-#include <complex>
-
-std::default_random_engine gen;
+// random number generator
+std::random_device rd;
+std::mt19937 gen(rd());
 std::uniform_real_distribution<double> real(-2,1); // real range
 std::uniform_real_distribution<double> imaginary(-1.5,1.5); // irrational range
+
+//displays the complex number supplied
+// void display(complex <double> c){
+//    std::cout << "The complex number is: ";
+//    std::cout << real(c) << '+' << imag(c) << 'i' << std::endl;
+// }
 
 
 
 int main(){
     double c_real = real(gen);
-    double c_imaginary = imaginary(gen);    
+    double c_imaginary = imaginary(gen); 
     std::complex<double> c = c_real + c_imaginary;
     std::cout << c;
+    // display(c);
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+### Luca does a caltech advanced c++ lab assignment
+
+given the header file mbrot.h i need to implement it in mbrot.cpp and then implement the buddhabrot
+
+
+The assignment is split into 3 parts lets see how far i get
